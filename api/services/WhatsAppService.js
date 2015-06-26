25219b262e39a4abeeca37e9b67fd681a11de0d6
Jiew Meng <jiewmeng@gmail.com>
2015-06-26 14:03:47 +0800

    Line break with slash n

--- api/services/WhatsAppService.js
+++ api/services/WhatsAppService.js
@@ -134,7 +134,7 @@ module.exports = {
 
 						var newContent = cards.map(function(card) {
 							return card.content;
-						}).join('. ');
+						}).join('\n ');
 
 						return Promise.all([
 							newContent,
